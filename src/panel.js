dbc6bc087d0dc9dae1f423d97ed47f2c9b199d57
Dipesh Bishwakarma <deepeesh471@gmail.com>
2020-12-15 16:15:12 +0545

    functional panel

--- src/panel.js
+++ src/panel.js
@@ -1,9 +1,22 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import './panel.css'
 class Covidpanel extends Component{
 	render(){
+		let item=this.props.data;
 		return(
-		<div>{this.props.data.country}
+		<div>
+			<div>
+				<p className='header'>{item.country}</p>
+				<div className='maindiv'>
+					<p className='datas'>Total cases: {item.cases}</p>
+					<p className='datas'>Total Deaths: {item.deaths}</p>
+					<p className='datas'>Total Recovered: {item.recovered}</p>
+					<p className='datas'>Active Cases: {item.active}</p>
+					<p className='datas'>New Cases: {item.todayCases}</p>
+					<p className='datas'>New Deaths: {item.todayDeaths}</p>
+				</div>
+			</div>
 		</div>
 		
 		);
