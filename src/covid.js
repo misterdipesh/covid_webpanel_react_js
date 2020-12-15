32df7cfe0cc9890b18f5de464a2a87c23c097a85
Dipesh Bishwakarma <deepeesh471@gmail.com>
2020-12-15 14:46:35 +0545

    successful api call

--- src/covid.js
+++ src/covid.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-const Covid=()=>{
+class Covid extends React.Component{
 
-	componentDidMount() {
-		this.getRemoteData();
-	};
 	constructor(props){
 		super(props);
 		this.state={
-			covidData:[]
+			covidData:[],
+			loading:true
 		}
 	}	
+	componentDidMount() {
+		this.getRemoteData();
+	}
 	
 
 	getRemoteData=()=>{
 		let self=this;
 		axios.get('https://coronavirus-19-api.herokuapp.com/countries').then(function(response){
-		self.setState(){
-			covidData:response.data
-		}
+		self.setState({
+			covidData:response.data,
+			loading:false
+			})
 		}).catch(function(error){
 		
 		});
 	};
-
+	render(){
 		return(
-		<div>
-			hello
-		</div>
+				<div>{this.state.loading?<div>Page is loading...</div>:
+				<div>{this.state.covidData.map((data)=><div>{data.country}</div>)}</div>}
+				</div>			   
 		);
+	}
 };
 export default Covid;
